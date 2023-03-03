c45f35105996cf784abdbd1f36133d4457ec4605
Jorge Piqueras García-Ligero <jorgepiqueras92@gmail.com>
2023-03-03 22:00:09 +0000

    feat:First commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,28 @@
 import React from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+
 
 //create your first component
-const Home = () => {
+
+
+const Home = (props) => {
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+  <>
+  <div className="all">
+  <div className="back ">
+    <div className="fas fa-clock"></div>
+    <div className="celda">0</div>
+    <div className="celda">0</div>
+    <div className="celda">{props.hours}</div>
+    <div className="celda">{props.minutes}</div>
+    <div className="celda">{props.seconds}</div>
+  </div>
+  </div>
+  </>
+			
 	);
-};
+	 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,35 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
-//render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+
+const contador =() =>{
+    seconds++; 
+    if (seconds>59){
+        minutes++;
+        seconds =0;
+    }
+    if (minutes >59){
+        minutes =0;
+        hours ++;
+    }
+
+
+
+   
+    ReactDOM.render(
+    <Home 
+    seconds={seconds}
+    minutes={minutes} 
+    hours={hours}
+    />, 
+    document.querySelector("#app"));
+}
+
+
+
+setInterval(() => {
+    contador()
+},1000)
